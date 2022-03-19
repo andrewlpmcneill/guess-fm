ff1aeb36dd93368b97ed4f46a8b11a0646822acc
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-19 17:05:32 -0400

    add station model and migration file

---
+++ backend/migrations/20220319205403-create-station.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Stations', {
+      id: {
+        allowNull: false,
+        autoIncrement: false,
+        primaryKey: true,
+        type: Sequelize.STRING
+      },
+      station_title: {
+        type: Sequelize.STRING
+      },
+      mp3_link: {
+        type: Sequelize.STRING
+      },
+      place_id: {
+        type: Sequelize.STRING
+      },
+      city: {
+        type: Sequelize.STRING
+      },
+      country: {
+        type: Sequelize.STRING
+      },
+      latitude: {
+        type: Sequelize.DECIMAL
+      },
+      longitude: {
+        type: Sequelize.DECIMAL
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Stations');
+  }
+};
