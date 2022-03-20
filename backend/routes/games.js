e8394072dcf49b4d70d574103937a3d132e70b92
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-20 15:06:42 -0400

    add gameController and link to game router

--- backend/routes/games.js
+++ backend/routes/games.js
@@ -1,9 +1,8 @@
 const express = require("express");
+const gameController = require("../controllers/gameController")
 const router = express.Router();
 
 
-router.get('/',(req, res) => {
-  res.json("this is the games");
-});
+router.get('/', gameController.demoGreeting);
 
 module.exports = router;
