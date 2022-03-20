3172a17c3ed6ad1a246ee17b99df49ee36a182e6
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-20 13:46:08 -0400

    add station routes template

--- backend/app.js
+++ backend/app.js
@@ -2,14 +2,15 @@ const express = require('express');
 const morgan = require('morgan');
 const { Sequelize } = require('sequelize');
 const db = require('./models');
+
 const PORT = 8080;
 const ENVIRONMENT = 'dev';
-
 const app = express();
 const sequelize = new Sequelize('guessfm', 'labber', 'labber', {
   host: 'localhost',
   dialect: 'postgres'
 });
+const stationRoutes  = require("./routes/stations")
 
 // middleware setup
 app.use(morgan(ENVIRONMENT));
@@ -23,6 +24,7 @@ app.use(express.urlencoded({
 app.get('/', (req, res) => {
   res.json({greetings: 'hello world'});
 });
+app.use('/stations/', stationRoutes)
 
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}`));
 
