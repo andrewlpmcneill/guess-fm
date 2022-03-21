b372e2ed044dccda28226c3c52d907720e68b424
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-21 18:54:03 -0400

    fix results component styling and component

--- frontend/src/components/Results/Results.jsx
+++ frontend/src/components/Results/Results.jsx
@@ -1,32 +1,38 @@
-import React, {useEffect} from "react";
-import Drawer from "@mui/material/Drawer";
+import React, { useEffect } from "react";
 import Guesses from "./Guesses";
+import GameScore from "./GameScore";
+import { IconButton, Drawer } from "@mui/material";
+import styles from "./Results.module.css"
 
 // const DUMMY_RESULT_DATA = 1;
 
-const drawerWidth = 240;
+const drawerWidth = 300;
 
 export default function Results(props) {
+  const { guesses, score } = props;
 
-  const {guesses, score} = props;
+  // const [open, setOpen] = useEffect(true);
 
-  // const [open, setOpen] = useEffect(true)
-
-  return <Drawer
-    className="results"
-    sx={{
-      width: drawerWidth,
-      flexShrink: 0,
-      '& .MuiDrawer-paper': {
+  return (
+    <Drawer
+      className={styles.drawer}
+      sx={{
         width: drawerWidth,
-      },
-    }}
-
-    variant="persistent"
-    anchor="right"
-    open={true}
-  >
-    <Guesses data={guesses}/>
-  </Drawer>
-
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          width: drawerWidth,
+        },
+        fontFamily: 'Wild World'
+      }}
+      variant="persistent"
+      anchor="right"
+      open={true}
+    >
+      <IconButton>
+        ▶
+      </IconButton>
+      <Guesses data={guesses} />
+      <GameScore data={score}/>
+    </Drawer>
+  );
 }
