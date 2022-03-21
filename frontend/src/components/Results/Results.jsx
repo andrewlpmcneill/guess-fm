1d5278fc490628ccba293d39a83f89b79615932d
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-21 15:17:25 -0400

    add dummy data

--- frontend/src/components/Results/Results.jsx
+++ frontend/src/components/Results/Results.jsx
@@ -1,5 +1,15 @@
 import React from "react";
 
+const DUMMY_GUESSES_DATA = [
+  {distanceAway: 4500, direction: "N"},
+  {distanceAway: 6000, direction: "S"},
+  {distanceAway: 3200, direction: "SW"},
+  {distanceAway: 1800, direction: "NE"},
+  {distanceAway: 0, direction: ""}
+]
+
+const DUMMY_RESULT_DATA = 1
+
 export default function Results(props) {
 
   return (
