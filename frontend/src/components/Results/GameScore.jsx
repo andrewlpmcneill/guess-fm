a538ff3feb1685ada44aa7b87c7f2702cc7e8337
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-21 18:52:26 -0400

    add gamescore result component

--- frontend/src/components/Results/GameScore.jsx
+++ frontend/src/components/Results/GameScore.jsx
@@ -1,10 +1,15 @@
 import React from "react";
+import styles from './GameScore.module.css'
 
 export default function GameScore(props) {
 
+  const score = props.data ? props.data: 0;
+
   return (
 
-    <div variant="" className="gameScore">
+    <div className={styles.scoreContainer}>
+      <h2 className={styles.scoreItem}>Current Score:</h2>
+      <h3 className={styles.scoreItem}>{score}/3</h3>
     </div>
 
   )
