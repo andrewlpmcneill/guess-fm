857a964063c96d37b9511bedf31591f66bc8ab82
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-21 16:11:23 -0400

    add guess template logic

--- frontend/src/components/Results/Guesses.jsx
+++ frontend/src/components/Results/Guesses.jsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Divider from "@mui/material/Divider";
+
 
 export default function Guesses(props) {
+  const guessesData = props.data;
+
+  const guesses = guessesData.map((guess) => (
+
+    <Fragment>
+      <ListItem>
+        <ListItemText primary={guess.distanceAway}/>
+      </ListItem>
+      <Divider/>
+    </Fragment>
+
+  ))
 
   return (
 
     <div variant="" className="guesses">
+      <h3>Guesses</h3>
+      <List>
+        {guesses}
+      </List>
     </div>
 
   )
