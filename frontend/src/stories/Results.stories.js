78d8db8e6ff3b0481ca2b47f5824203471ea99e0
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-21 18:54:54 -0400

    add sad path result stories

--- frontend/src/stories/Results.stories.js
+++ frontend/src/stories/Results.stories.js
@@ -2,16 +2,18 @@ import Results from "../components/Results/Results";
 import { storiesOf } from "@storybook/react";
 
 const DUMMY_GUESSES_DATA = [
-  { distanceAway: 4500, direction: "N" },
-  { distanceAway: 6000, direction: "S" },
-  { distanceAway: 3200, direction: "SW" },
-  { distanceAway: 1800, direction: "NE" },
-  { distanceAway: 0, direction: "" },
+  { country:"Singapore", distanceAway: 4500, direction: "N" },
+  { country:"Zimbabwe", distanceAway: 6000, direction: "S" },
+  { country:"Monstserrat", distanceAway: 3200, direction: "SW" },
+  { country:"Italy", distanceAway: 1800, direction: "NE" },
+  { country:"Norway", distanceAway: 0, direction: "" },
 ];
 
+const DUMMY_SCORE_DATA = 2
 
 storiesOf("Results menu", module)
-  .addParameters({
-    
-  })
-  .add("Base", () => <Results guesses={DUMMY_GUESSES_DATA}></Results>);
+  .addParameters({})
+  .add("Base", () => <Results guesses={DUMMY_GUESSES_DATA} score={DUMMY_SCORE_DATA}></Results>)
+  .add("first round no guesses", ()=> <Results guesses={null} score={null}></Results>)
+  .add("first round no score", ()=> <Results guesses={DUMMY_GUESSES_DATA} score={null}></Results>)
+  .add("new round no guesses", ()=> <Results guesses={null} score={null}></Results>);
