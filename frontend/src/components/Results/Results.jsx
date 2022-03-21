a41a2a45988fa0da40f1c6f9f8df351e5e4a7120
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-21 16:11:47 -0400

    add dummy data into results story

--- frontend/src/components/Results/Results.jsx
+++ frontend/src/components/Results/Results.jsx
@@ -1,19 +1,14 @@
 import React, {useEffect} from "react";
 import Drawer from "@mui/material/Drawer";
-
-// const DUMMY_GUESSES_DATA = [
-//   { distanceAway: 4500, direction: "N" },
-//   { distanceAway: 6000, direction: "S" },
-//   { distanceAway: 3200, direction: "SW" },
-//   { distanceAway: 1800, direction: "NE" },
-//   { distanceAway: 0, direction: "" },
-// ];
+import Guesses from "./Guesses";
 
 // const DUMMY_RESULT_DATA = 1;
 
 const drawerWidth = 240;
 
 export default function Results(props) {
+
+  const {guesses, score} = props;
 
   // const [open, setOpen] = useEffect(true)
 
@@ -31,18 +26,7 @@ export default function Results(props) {
     anchor="right"
     open={true}
   >
-    <div>
-      <p>guess1</p>
-    </div>
-    <div>
-      <p>guess2</p>
-    </div>
-    <div>
-      <p>guess3</p>
-    </div>
-    <div>
-      <p>guess4</p>
-    </div>
+    <Guesses data={guesses}/>
   </Drawer>
 
 }
--- frontend/src/stories/Results.stories.js
+++ frontend/src/stories/Results.stories.js
@@ -1,8 +1,17 @@
 import Results from "../components/Results/Results";
 import { storiesOf } from "@storybook/react";
 
+const DUMMY_GUESSES_DATA = [
+  { distanceAway: 4500, direction: "N" },
+  { distanceAway: 6000, direction: "S" },
+  { distanceAway: 3200, direction: "SW" },
+  { distanceAway: 1800, direction: "NE" },
+  { distanceAway: 0, direction: "" },
+];
+
+
 storiesOf("Results menu", module)
   .addParameters({
     
   })
-  .add("Base", () => <Results></Results>);
+  .add("Base", () => <Results guesses={DUMMY_GUESSES_DATA}></Results>);
