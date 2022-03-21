486675491e3dcd6523c6c0e26be0fec93207fc2d
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-21 17:37:39 -0400

    refactor GuessesItem to contain country data

---
+++ frontend/src/components/Results/GuessesItem.jsx
@@ -0,0 +1,23 @@
+import React, { Fragment } from "react";
+import styles from './GuessesItem.module.css';
+
+
+export default function Guesses(props) {
+  const guessData = props.data;
+
+  const directionClue = guessData.direction ? <p>{guessData.direction}</p> : <p>✔</p>
+
+  return (
+    <div className={styles.guessItem}>
+      <div className={`${styles.guessItem__column} ${styles.country}`}>
+        <p>{guessData.country}</p>
+      </div>
+      <div className={`${styles.guessItem__column} ${styles.distanceAway}`}>
+        <p>{guessData.distanceAway}km </p>
+      </div>
+      <div className={`${styles.guessItem__column} ${styles.direction}`}>
+        {directionClue}
+      </div>
+    </div>
+  );
+}
