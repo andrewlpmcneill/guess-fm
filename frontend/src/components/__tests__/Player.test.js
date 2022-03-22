dd199e84d07ab2ecda9fb6f11c94b3414cd8c66b
Andrew McNeill <andrew@andrewmc.ca>
2022-03-22 15:16:13 -0300

    imports the correct component

--- frontend/src/components/__tests__/Player.test.js
+++ frontend/src/components/__tests__/Player.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render, cleanup, fireEvent } from "@testing-library/react";
-import Player from '../Player/Player'
+import Player from '../Player/index'
 
 afterEach(cleanup);
 
