6dd45573c2b7726a563eb340d37d57026315bb45
Andrew McNeill <andrew@andrewmc.ca>
2022-03-22 15:17:47 -0300

    removes unnecessary react dependencies

--- frontend/src/components/__tests__/Volume.test.js
+++ frontend/src/components/__tests__/Volume.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, cleanup, fireEvent } from "@testing-library/react";
+import { render, cleanup, fireEvent, screen } from "@testing-library/react";
 import Volume from '../Player/Volume'
 
 afterEach(cleanup);
