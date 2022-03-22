3d343745770787d79ac80c92e172ea7f0e16333e
Andrew McNeill <andrew@andrewmc.ca>
2022-03-22 11:35:01 -0300

    adds version 1 of volume component

--- frontend/src/components/Player/Volume.jsx
+++ frontend/src/components/Player/Volume.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import Stack from '@mui/material/Stack';
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
+import VolumeDown from '@mui/icons-material/VolumeDown'
+import VolumeUp from '@mui/icons-material/VolumeUp'
 
 export default function Volume(props) {
 
   return (
 
-    <div variant="" className="volume">
-    </div>
-
+    <Box sx={{ width: 220 }}>
+      <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
+        <VolumeDown />
+        <Slider aria-label="Volume" value={props.value} onChange={props.onChange} />
+        <VolumeUp />
+      </Stack>
+    </Box>
   )
 
 }
