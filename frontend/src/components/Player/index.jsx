f66d2a232b29deb78ae491837967323b868f1030
Andrew McNeill <andrew@andrewmc.ca>
2022-03-22 15:14:26 -0300

    adds effect hook for initial volume, adds MUI stack as flex container for children

--- frontend/src/components/Player/index.jsx
+++ frontend/src/components/Player/index.jsx
@@ -1,4 +1,6 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Stack from '@mui/material/Stack'
+import Button from '@mui/material/Button'
 import PlayButton from "./PlayButton";
 import Volume from "./Volume";
 
@@ -7,6 +9,10 @@ export default function Player(props) {
   const [playing, setPlaying] = useState(true);
   const [value, setValue] = useState(30);
   const player = document.getElementById("mp3Player");
+
+  useEffect(() => {
+    document.getElementById("mp3Player").volume = 0.3;
+  }, [])
 
   const click = () => {
     if (playing) {
@@ -26,11 +32,20 @@ export default function Player(props) {
 
   return (
 
-    <div className="playerContainer">
+    <Stack
+      direction="row"
+      m="0 2em"
+      spacing={2}
+      alignItems="center"
+      justifyContent="space-between"
+    >
       <PlayButton onClick={click} icon={playing ? "playing" : "paused"} />
-      <audio autoPlay id="mp3Player" src="http://radio.garden/api/ara/content/listen/Z8RjDwNU/channel.mp3" type="audio/mp3" />
-      <Volume className="volumeSlider" value={value} onChange={handleChange}/>
-    </div>
+      <audio id="mp3Player" src="http://radio.garden/api/ara/content/listen/Z8RjDwNU/channel.mp3" type="audio/mp3" />
+      <Volume value={value} onChange={handleChange}/>
+      <Button fullWidth variant="contained">
+        Guess
+      </Button>
+    </Stack>
 
     
 
