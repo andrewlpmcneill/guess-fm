92601036776d11120c95557c85b4808a723b0975
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-22 16:41:03 -0400

    add drawer to app, add conditional drawer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import Map from './components/Map'
 import './App.css';
 import Player from './components/Player/index';
+import Results from './components/Results/index'
+import { Box } from '@mui/material';
 
 function App() {
 
@@ -9,9 +11,10 @@ function App() {
   return (
     <div className="App">
       <h3>GUESS FM</h3>
-      <div>
-        <Map />  
-      </div>
+      <Box sx={{position: 'relative'}}>
+        <Map />
+        <Results/>
+      </Box>
       <Player />
     </div>
   );
