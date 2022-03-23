735807eaaa6ca02137e76cf9c174a35bea3da398
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-23 12:38:33 -0400

    fix useDisplayMode to be a function expression

--- frontend/src/hooks/useDisplayMode.js
+++ frontend/src/hooks/useDisplayMode.js
@@ -1,11 +1,11 @@
 import React, { useState } from "react";
 
-export default useDisplay = (initial) => {
+export default function useDisplayMode(initial) {
   const [open, setOpen] = useState(initial);
 
   const toggleOpen = () => {
     setOpen((prev) => !prev);
   };
 
-  return {open, setOpen, toggleOpen}
-};
+  return [ open, setOpen, toggleOpen ];
+}
