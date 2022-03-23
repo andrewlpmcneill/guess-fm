3d1af7c253e9c6936d94367ddf844149ab476c05
Seamus McGill <seamusmcgill@gmail.com>
2022-03-23 10:50:16 -0400

    Clean up / fix conditional rendering

--- frontend/src/components/Dialog/index.jsx
+++ frontend/src/components/Dialog/index.jsx
@@ -1,54 +1,18 @@
-import { useState } from 'react';
-import { styled } from '@mui/material/styles';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import Typography from '@mui/material/Typography';
-import DialogButton from './DialogButton'
 import Instructions from './Instructions';
 import Announcement from './Announcement';
 import GameOver from './GameOver'
 
-// Alter padding and styling of the content and bottom actions section of materialUI component
-const CustomDialog = styled(Dialog)(({ theme }) => ({
-  '& .MuiDialogContent-root': {
-    padding: theme.spacing(2),
-    textAlign: 'center'
-  },
-  '& .MuiDialogActions-root': {
-    paddingBottom: theme.spacing(2),
-    margin: 'auto'
-  },
-}));
-
-// Customize the DialogTitle component
-const CustomDialogTitle = (props) => {
-  const { title } = props;
-
-  return (
-    // Centre with margin auto, add padding, customize fontFamily
-    // **CHANGE FONT FAMILY CUSTOMIZATION IN THEME**
-    <DialogTitle sx={{ m: 'auto', p: 2, fontFamily: 'Wild World' }} >
-      {title}
-    </DialogTitle>
-  );
-};
-
-export default function CustomizedDialog(props) {
+export default function Dialog(props) {
   // Props passed in to render Dialog box
-  const { round, autoplay, resetGame, nextRound } = props;
-  // Track whether or not the dialog box is open
-  const [open, setOpen] = useState(true);
-
-  const handleClose = () => {
-    setOpen(false);
-  };
+  const { round, autoplay, resetGame, nextRound, clearGuesses } = props;
 
   return (
     <div>
-      {round === 0 && <Instructions round={round} nextRound={nextRound} />}
-      {round > 0 && round < 4 && <Announcement round={round} autoplay={autoplay} />}
-      {round > 3 && <GameOver />} 
+      {round === 0 && <Instructions resetGame={resetGame} round={round} nextRound={nextRound} />}
+      {round === 1 && <Announcement round={round} autoplay={autoplay} clearGuesses={clearGuesses} />}
+      {round === 2 && <Announcement open={true} round={round} autoplay={autoplay} clearGuesses={clearGuesses}/>}
+      {round === 3 && <Announcement open={true} round={round} autoplay={autoplay} clearGuesses={clearGuesses}/>}
+      {round > 3 && <GameOver resetGame={resetGame}/>} 
     </div>
   );
 }
