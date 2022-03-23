4253522be15f05670621edb5f89488c59dfebd1d
Andrew McNeill <andrew@andrewmc.ca>
2022-03-23 19:08:04 -0300

    adds setAudio prop, passes it to relevant children

--- frontend/src/components/Dialog/index.jsx
+++ frontend/src/components/Dialog/index.jsx
@@ -4,14 +4,14 @@ import GameOver from './GameOver'
 
 export default function Dialog(props) {
   // Props passed in to render Dialog box
-  const { round, play, pause, startGame, nextRound, clearGuesses } = props;
+  const { round, play, pause, startGame, nextRound, clearGuesses, setAudio } = props;
 
   return (
     <div>
-      {round === 0 && <Instructions startGame={startGame} round={round} nextRound={nextRound} />}
+      {round === 0 && <Instructions startGame={startGame} round={round} nextRound={nextRound} setAudio={setAudio}/>}
       {round === 1 && <Announcement round={round} play={play} clearGuesses={clearGuesses} />}
-      {round === 2 && <Announcement open={true} round={round} play={play} clearGuesses={clearGuesses}/>}
-      {round === 3 && <Announcement open={true} round={round} play={play} clearGuesses={clearGuesses}/>}
+      {round === 2 && <Announcement open={true} round={round} play={play} clearGuesses={clearGuesses} setAudio={setAudio}/>}
+      {round === 3 && <Announcement open={true} round={round} play={play} clearGuesses={clearGuesses} setAudio={setAudio}/>}
       {round > 3 && <GameOver startGame={startGame} pause={pause} />} 
     </div>
   );
