c55ad2b4c6be3e074728a4d103e95f77cc037e8b
Seamus McGill <seamusmcgill@gmail.com>
2022-03-22 22:50:24 -0400

    Move round announcement to dialog component

---
+++ frontend/src/components/Dialog/Announcement.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogTitle from '@mui/material/DialogTitle';
+import Slide from '@mui/material/Slide';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
+
+export default function Announcement(props) {
+  // Set open to true for testing purposes
+  const [open, setOpen] = React.useState(true);
+  const { round, onClick } = props;
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      {/* Customize materialUI dialog box to  slide up and size*/}
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        aria-describedby="alert-dialog-slide-description"
+        fullWidth={true}
+        maxWidth={'xs'}
+        onClick={onClick}
+      >
+        {/* Dynamically generate round number */}
+        <DialogTitle sx={{m: 'auto', fontFamily: 'Wild World'}}>{`ROUND ${round}`}</DialogTitle>
+        <DialogActions sx={{p: 2}}>
+          <Button onClick={handleClose} variant="contained" sx={{m: 'auto', fontFamily: 'Wild World'}}>START</Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
