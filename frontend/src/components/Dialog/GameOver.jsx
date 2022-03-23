bec02634a4e9a429221845ecf092bc2a69f0759e
Seamus McGill <seamusmcgill@gmail.com>
2022-03-22 22:50:38 -0400

    Move end of game dialog to own component

---
+++ frontend/src/components/Dialog/GameOver.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import Typography from '@mui/material/Typography';
+import DialogButton from './DialogButton'
+
+// Alter padding and styling of the content and bottom actions section of materialUI component
+const CustomDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(2),
+    textAlign: 'center'
+  },
+  '& .MuiDialogActions-root': {
+    paddingBottom: theme.spacing(2),
+    margin: 'auto'
+  },
+}));
+
+// Customize the DialogTitle component
+const CustomDialogTitle = (props) => {
+  const { title } = props;
+
+  return (
+    // Centre with margin auto, add padding, customize fontFamily
+    // **CHANGE FONT FAMILY CUSTOMIZATION IN THEME**
+    <DialogTitle sx={{ m: 'auto', p: 2, fontFamily: 'Wild World' }} >
+      {title}
+    </DialogTitle>
+  );
+};
+
+export default function GameOver(props) {
+  // Props passed in to render Dialog box
+  const { round, autoplay, resetGame } = props;
+  // Track whether or not the dialog box is open
+  const [open, setOpen] = useState(true);
+
+  const handleClose = () => {
+    resetGame();
+    setOpen(false);
+  };
+
+  return (
+    <div>
+      <CustomDialog
+        open={open}
+        fullWidth
+      >
+        <CustomDialogTitle id="customized-dialog-title">
+          {"WELCOME TO GUESS FM"}
+        </CustomDialogTitle>
+        {/* Add margin around content */}
+        <DialogContent sx={{ m: 1}}>
+          <Typography >
+            {"Your final score is X/3."}
+          </Typography>
+        </DialogContent>
+        <DialogButton onClick={handleClose}>{"PLAY AGAIN"}</DialogButton>
+      </CustomDialog>
+    </div>
+  );
+}
