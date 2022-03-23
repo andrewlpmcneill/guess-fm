8f154ab4087638183c8ae702d2f00ee75238bc40
Andrew McNeill <andrew@andrewmc.ca>
2022-03-23 16:09:26 -0300

    moves guess validation into app, fixes props accordingly

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Map from "./components/Map";
 import "./App.css";
 import Player from "./components/Player/index";
@@ -9,6 +9,9 @@ import useDisplayMode from "./hooks/useDisplayMode";
 import useRoundData from "./hooks/useRoundData";
 import useGuessesData from "./hooks/useGuessesData";
 import usePlayerData from "./hooks/usePlayerData";
+import getDistanceFromLatLonInKm from "./helpers/getDistanceFromCoords";
+import axios from "axios";
+import { getCompassDirection } from "geolib";
 
 function App() {
   // Import state and functionality from useRoundData hook
@@ -21,6 +24,50 @@ function App() {
     }
   };
 
+  const [gameData, setGameData] = useState([])
+
+  useEffect(() => {
+    axios.get('/stations')
+      .then(response => {
+        console.log(response.data);
+        setGameData(response.data);
+      })
+  }, [])
+
+  const lookup = require('country-code-lookup')
+
+  const [country, setCountry] = useState('');
+  const [coords, setCoords] = useState([]);
+
+
+  const validateGuess =  () => {
+    
+    let distanceAway, guessCountry, direction;
+    
+    if (lookup.byIso(country).country === gameData[0].country) {
+      distanceAway = 0;
+      guessCountry = gameData[0].country
+      direction = ""
+    }
+    else {
+      distanceAway = Math.round(getDistanceFromLatLonInKm(gameData[0].longitude, gameData[0].latitude, coords[0], coords[1]));
+      guessCountry = lookup.byIso(country).country;
+      direction = getCompassDirection(
+        // guess coords
+        { latitude: coords[0], longitude: coords[1] },
+        // answer coords
+        // Latitude = longitude and longitude = latitude (RADIO GARDEN ERROR)
+        { latitude: gameData[0].longitude, longitude: gameData[0].latitude });
+    }
+    return({
+      id: 1,
+      distanceAway: distanceAway,
+      direction: direction,
+      country: guessCountry,
+    })
+
+  }
+
   // RESULTS STATE
   const [isDrawerOpen, setIsDrawerOpen, toggleDrawer] = useDisplayMode(false);
 
@@ -32,13 +79,12 @@ function App() {
 
   // PLAYER STATE
   const { playing, value, click, handleChange, play, pause } = usePlayerData();
-  //
 
   return (
     <div className="App">
       <h3>GUESS FM</h3>
       <Box sx={{ position: "relative", overflow: "hidden" }}>
-        <Map setMapData={setMapData} />
+        <Map setMapData={setMapData} gameData={gameData} setCountry={setCountry} country={country} coords={coords} setCoords={setCoords} />
         <Dialog
           round={round}
           play={play}
@@ -58,7 +104,8 @@ function App() {
           handleClick={click}
           handleChange={handleChange}
           addGuess={addGuess}
-          guess={mapData}
+          gameData={gameData}
+          validateGuess={validateGuess}
           updateRoundStatus={updateRoundStatus}
         />
       </Box>
