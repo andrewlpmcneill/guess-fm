5768dcf4c09c187364a4359298cedcb7bc329f0e
Seamus McGill <seamusmcgill@gmail.com>
2022-03-24 14:57:42 -0400

    Add startRound and startGame functions to onClick

--- frontend/src/components/Dialog/GameOver.jsx
+++ frontend/src/components/Dialog/GameOver.jsx
@@ -7,7 +7,7 @@ import DialogButton from './DialogButton'
 
 export default function GameOver(props) {
   // Props passed in to render Dialog box
-  const { round, startGame, pause, setAudio, setRound, setScore, score } = props;
+  const { round, pause, setAudio, setRound, setScore, score } = props;
   // Track whether or not the dialog box is open
   const [open, setOpen] = useState(true);
 
--- frontend/src/components/Dialog/index.jsx
+++ frontend/src/components/Dialog/index.jsx
@@ -1,18 +1,77 @@
-import Instructions from './Instructions';
-import Announcement from './Announcement';
-import GameOver from './GameOver'
+import Instructions from "./Instructions";
+import Announcement from "./Announcement";
+import GameOver from "./GameOver";
 
 export default function Dialog(props) {
   // Props passed in to render Dialog box
-  const { round, play, pause, startGame, nextRound, clearGuesses, setAudio, setRound, game, setScore, score } = props;
+  const {
+    state,
+    setState,
+    round,
+    play,
+    pause,
+    nextRound,
+    clearGuesses,
+    setAudio,
+    setRound,
+    game,
+    setScore,
+    score,
+    startGame,
+    startRound,
+  } = props;
 
   return (
     <div>
-      {round === 0 && <Instructions startGame={startGame} round={round} nextRound={nextRound} setAudio={setAudio} setRound={setRound} game={game}/>}
-      {round === 1 && <Announcement round={round} play={play} clearGuesses={clearGuesses} />}
-      {round === 2 && <Announcement open={true} round={round} play={play} clearGuesses={clearGuesses} setAudio={setAudio}/>}
-      {round === 3 && <Announcement open={true} round={round} play={play} clearGuesses={clearGuesses} setAudio={setAudio}/>}
-      {round > 3 && <GameOver startGame={startGame} pause={pause} setAudio={setAudio} setRound={setRound} setScore={setScore} score={score}/>} 
+      {round === 0 && (
+        <Instructions
+          state={state}
+          setState={setState}
+          startGame={startGame}
+          round={round}
+          nextRound={nextRound}
+          setAudio={setAudio}
+          setRound={setRound}
+          game={game}
+        />
+      )}
+      {round === 1 && (
+        <Announcement
+          round={round}
+          play={play}
+          clearGuesses={clearGuesses}
+          startRound={startRound}
+        />
+      )}
+      {round === 2 && (
+        <Announcement
+          open={true}
+          round={round}
+          play={play}
+          clearGuesses={clearGuesses}
+          setAudio={setAudio}
+          startRound={startRound}
+        />
+      )}
+      {round === 3 && (
+        <Announcement
+          open={true}
+          round={round}
+          play={play}
+          clearGuesses={clearGuesses}
+          setAudio={setAudio}
+          startRound={startRound}
+        />
+      )}
+      {round > 3 && (
+        <GameOver
+          pause={pause}
+          setAudio={setAudio}
+          setRound={setRound}
+          setScore={setScore}
+          score={score}
+        />
+      )}
     </div>
   );
 }
