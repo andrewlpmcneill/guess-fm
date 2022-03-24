0306453c19ff93bf6023cfda5a7d6e4cd2365d20
Seamus McGill <seamusmcgill@gmail.com>
2022-03-24 14:56:14 -0400

    Update to get all results instead of one specific result

--- backend/routes/results.js
+++ backend/routes/results.js
@@ -2,7 +2,7 @@ const express = require("express");
 const resultController = require("../controllers/resultController");
 const router = express.Router();
 
-router.get("/", resultController.getResult);
+router.get("/", resultController.getResults);
 
 router.patch('/', resultController.updateResults);
 router.post("/", resultController.createResult);
