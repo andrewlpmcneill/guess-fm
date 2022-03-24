342fe32e9dbf6aea58720bc1a09c02cbce9d88e2
Andrew McNeill <andrew@andrewmc.ca>
2022-03-24 13:13:20 -0300

    adds updateResults route action

--- backend/controllers/resultController.js
+++ backend/controllers/resultController.js
@@ -2,8 +2,28 @@ const { Sequelize } = require('../models');
 const db = require('../models');
 const result = db.Result;
 
-const demoGreeting = (req,res) => {
-  res.json({greetings: "This is coming from the results controller"});
+const updateResults = async(req, res) => {
+  try {
+    const newResults = req.body.results;
+    const gameID = req.body.game_id;
+    const userID = req.body.user_id;
+    const updatedResults = await result.update(
+      { results: newResults },
+      {
+        where: {
+          // eslint-disable-next-line camelcase
+          user_id: userID,
+          // eslint-disable-next-line camelcase
+          game_id: gameID
+        }
+      }
+    );
+    res.json(updatedResults);
+
+
+  } catch (err) {
+    console.log(err);
+  }
 };
 
-module.exports = { demoGreeting };
+module.exports = { updateResults };
