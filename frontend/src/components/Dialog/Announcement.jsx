c7b2b65f1d240ebcee919bf6340399c7d62da497
Seamus McGill <seamusmcgill@gmail.com>
2022-03-25 15:55:50 -0400

    Updates props

--- frontend/src/components/Dialog/Announcement.jsx
+++ frontend/src/components/Dialog/Announcement.jsx
@@ -1,11 +1,10 @@
-import { useState, useEffect, forwardRef } from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import { CircularProgress } from '@mui/material';
-
+import { useState, useEffect, forwardRef } from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import { CircularProgress } from "@mui/material";
 
 // Sliding up transition component, passed to dialog box
 const Transition = forwardRef(function Transition(props, ref) {
@@ -15,27 +14,37 @@ const Transition = forwardRef(function Transition(props, ref) {
 export default function Announcement(props) {
   // Set open to true for testing purposes
   const [open, setOpen] = useState(true);
-  const { round, onClick, play, clearGuesses, createRound, gameData, modelState } = props;
+  const {
+    onClick,
+    play,
+    clearGuesses,
+    createRound,
+    modelState,
+    gameData
+  } = props;
 
   // On close of round announcement - play audio and clear previous guesses
   const handleClose = () => {
     setOpen(false);
-    // GAME DATA IS STATIONS
-    createRound(modelState.userId, modelState.gameId, gameData[round - 1].id)
-    .then(() => {
+    createRound(
+      modelState.userId,
+      modelState.gameId,
+      // Can we clean this up?
+      gameData.stations[gameData.round - 1].id
+    ).then(() => {
       play();
       clearGuesses();
     });
   };
 
   // Set short buffer on play button to give audio player time to load
-  const [buffer ,setBuffer] = useState(true);
+  const [buffer, setBuffer] = useState(true);
 
   useEffect(() => {
     setTimeout(() => {
       setBuffer(false);
-    },1000);
-  },[]);
+    }, 1000);
+  }, []);
 
   return (
     <div>
@@ -45,16 +54,25 @@ export default function Announcement(props) {
         TransitionComponent={Transition}
         aria-describedby="alert-dialog-slide-description"
         fullWidth={true}
-        maxWidth={'xs'}
+        maxWidth={"xs"}
         onClick={onClick}
       >
         {/* Dynamically generate round number */}
-        <DialogTitle sx={{m: 'auto', fontFamily: 'Wild World'}}>{`ROUND ${round}`}</DialogTitle>
-        <DialogActions sx={{p: 2}}>
-          { buffer 
-            ? <CircularProgress sx={{m: 'auto'}} />
-            : <Button onClick={handleClose} variant="contained" sx={{m: 'auto', fontFamily: 'Wild World'}}>START</Button>
-          }
+        <DialogTitle
+          sx={{ m: "auto", fontFamily: "Wild World" }}
+        >{`ROUND ${gameData.round}`}</DialogTitle>
+        <DialogActions sx={{ p: 2 }}>
+          {buffer ? (
+            <CircularProgress sx={{ m: "auto" }} />
+          ) : (
+            <Button
+              onClick={handleClose}
+              variant="contained"
+              sx={{ m: "auto", fontFamily: "Wild World" }}
+            >
+              START
+            </Button>
+          )}
         </DialogActions>
       </Dialog>
     </div>
