8286e016f4b31457076353100f2fc3e1df011db3
Seamus McGill <seamusmcgill@gmail.com>
2022-03-26 12:21:49 -0400

    Logs user in

--- frontend/src/components/LoginButton.jsx
+++ frontend/src/components/LoginButton.jsx
@@ -1,12 +1,14 @@
 import { useAuth0 } from '@auth0/auth0-react';
 import Button from '@mui/material/Button';
-import { process_params } from 'express/lib/router';
+import useGameData from '../hooks/useGameData';
 
 export default function LoginButton(props) {
   const { loginWithRedirect } = useAuth0();
   return (
     <Button variant="contained" sx={{fontFamily: 'Wild World'}}
-      onClick={() => loginWithRedirect()}
+      onClick={() => {
+        loginWithRedirect()
+      }}
     >
       {props.children}
     </Button>
