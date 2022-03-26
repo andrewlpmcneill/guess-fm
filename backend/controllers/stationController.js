b5e63ff601062215c40ec90ac3721f08a5979dee
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-25 22:05:32 -0400

    refactors generateStations, increasing variability

--- backend/controllers/stationController.js
+++ backend/controllers/stationController.js
@@ -1,46 +1,64 @@
-const { Sequelize, sequelize } = require('../models');
-const db = require('../models');
+const { Sequelize, sequelize } = require("../models");
+const db = require("../models");
 const station = db.Station;
 
-
-const randomStations = async(req, res) => {
+const randomStations = async (req, res) => {
   try {
     const randomStations = [];
 
     //retrieves three random stations
-    const randomCountries = await (station).findAll({
-      attributes:['country'],
+    const randomCountries = await station.findAll({
+      attributes: ["country"],
       distinct: true,
       raw: true, //grabs only the data
-      group: 'country',
-      having: sequelize.literal('COUNT(*) > 5'),
-      order: Sequelize.literal('random()'),
-      limit: 3
+      group: "country",
+      having: sequelize.literal("COUNT(*) > 5"),
+      order: Sequelize.literal("random()"),
+      limit: 3,
     });
 
     //adds station data and appends to the array above
     await Promise.all(
-      randomCountries.map( async (randomCountry) => {
-        const randomStation = await (station).findAll({
-          where:{
-            country: randomCountry.country
+      randomCountries.map(async (randomCountry) => {
+        const randomStation = await station.findAll({
+          where: {
+            country: randomCountry.country,
           },
           raw: true,
-          order: Sequelize.literal('random()'),
-          limit: 1
-        })
+          order: Sequelize.literal("random()"),
+          limit: 1,
+        });
         randomStations.push(randomStation[0]);
       })
-    )
-    
-    res.json(randomStations)
+    );
 
-
+    res.json(randomStations);
   } catch (err) {
     console.log(err);
+  }
+};
+
+const replaceStation = async(req, res) => {
+
+  try{
+      const country = req.body.country;
+    
+      const substituteStation = await station.findAll({
+        where: {
+          country: country,
+        },
+        raw: true,
+        order: Sequelize.literal("random()"),
+        limit: 1,
+      });
+      
+      console.log(substituteStation)
+      res.json(substituteStation);
+    
+  }catch(err){
+    console.log(err)
   }
 
 };
 
-module.exports = { randomStations };
-
+module.exports = { randomStations, replaceStation };
