af54d50e5aef0de6531f2d122ede512003ea576e
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-26 23:01:18 -0400

    changes correct guess display on results distanceAway column to display a check mark

--- frontend/src/components/Results/GuessesItem.jsx
+++ frontend/src/components/Results/GuessesItem.jsx
@@ -3,12 +3,13 @@ import Paper from "@mui/material/Paper";
 import Stack from "@mui/material/Stack";
 import { styled } from "@mui/material/styles";
 import getDirectionIcon from "../../helpers/getDirectionIcon";
+import CheckIcon from '@mui/icons-material/Check';
 
 export default function Guesses(props) {
   const guessData = props.data;
 
   //returns an icon component
-  const distance = guessData.isCorrect ? 0 : guessData.distanceAway;
+  const distance = guessData.isCorrect ? <CheckIcon/> : `${guessData.distanceAway}km`;
   const directionClue = getDirectionIcon(guessData.direction);
 
   const Item = styled(Paper)(({ theme }) => ({
@@ -27,7 +28,7 @@ export default function Guesses(props) {
       sx={{ marginBottom: "5px", padding: "5px" }}
     >
       <Item sx={{ width: "55%" }}>{guessData.country}</Item>
-      <Item sx={{ width: "30%" }}>{distance}km</Item>
+      <Item sx={{ width: "30%" }}>{distance}</Item>
       <Item sx={{ width: "15%" }}>{directionClue}</Item>
     </Stack>
   );
