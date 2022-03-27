c4d8c7caa3ccd2285db47085409a040b5fcb93a7
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-26 23:37:03 -0400

    formats annoucements component

--- frontend/src/components/Dialog/Announcement.jsx
+++ frontend/src/components/Dialog/Announcement.jsx
@@ -5,7 +5,7 @@ import DialogActions from "@mui/material/DialogActions";
 import DialogTitle from "@mui/material/DialogTitle";
 import Slide from "@mui/material/Slide";
 import { CircularProgress, DialogContent, Typography } from "@mui/material";
-import getTicksWithDistance from "../../helpers/getTicksWithDistance"
+import getTicksWithDistance from "../../helpers/getTicksWithDistance";
 
 // Sliding up transition component, passed to dialog box
 const Transition = forwardRef(function Transition(props, ref) {
@@ -15,43 +15,47 @@ const Transition = forwardRef(function Transition(props, ref) {
 export default function Announcement(props) {
   // Set open to true for testing purposes
   const [open, setOpen] = useState(true);
-  const {
-    onClick,
-    play,
-    clearGuesses,
-    createRound,
-    modelState,
-    gameData
-  } = props;
+  const { onClick, play, clearGuesses, createRound, modelState, gameData } =
+    props;
 
+  //conditional text display logic for the button
+  const buttonTitle =
+    gameData.round === 1 ? "START" : `START ROUND ${gameData.round}`;
 
-  //dialog box triggers on correct guess or after 5guesses
-  const buttonTitle = gameData.round === 1? "START" : `START ROUND ${gameData.round}`
-  const answer = gameData.stations[gameData.round -2]
+  
+  const currentStation = gameData.stations[gameData.round - 2];
   let roundTitle;
   let roundAnswer = "";
   let pinDistanceAway;
   let distanceAwayVisual;
-  
 
-
-  if(gameData.round === 1){
-    roundTitle = "ROUND 1"
-  }else if (gameData.guesses.length >= 1 && gameData.guesses[gameData.guesses.length -1 ].isCorrect){
+  //fix this logic
+  //can not define variables outside of conditions as undefined will throw errors
+  if (gameData.round === 1) {
+    roundTitle = "ROUND 1";
+  } else if (
+    gameData.guesses.length >= 1 &&
+    gameData.guesses[gameData.guesses.length - 1].isCorrect
+  ) {
     // let dataRound = gameData.round === 2 ? 1 : 2;
-    roundTitle = "NICE ONE!"
-    roundAnswer = `You got it in ${gameData.guesses.length} guesses. `
-    pinDistanceAway = gameData.guesses[gameData.guesses.length - 1].distanceAway
-    distanceAwayVisual = `📍${getTicksWithDistance(gameData.guesses[gameData.guesses.length - 1].distanceAway)}🌎`
-
-  }else if(gameData.guesses.length > 4 && !gameData.guesses[gameData.guesses.length -1 ].isCorrect){
-    roundTitle = "OOPS... NOT QUITE"
-    pinDistanceAway = gameData.guesses[gameData.guesses.length - 1].distanceAway
-    distanceAwayVisual = `📍${getTicksWithDistance(gameData.guesses[gameData.guesses.length - 1].distanceAway)}🌎`
+    roundTitle = "NICE ONE!";
+    roundAnswer = `You got it in ${gameData.guesses.length} guesses. `;
+    pinDistanceAway =
+      gameData.guesses[gameData.guesses.length - 1].distanceAway;
+    distanceAwayVisual = `📍${getTicksWithDistance(
+      gameData.guesses[gameData.guesses.length - 1].distanceAway
+    )}🌎`;
+  } else if (
+    gameData.guesses.length > 4 &&
+    !gameData.guesses[gameData.guesses.length - 1].isCorrect
+  ) {
+    roundTitle = "OOPS... NOT QUITE";
+    pinDistanceAway =
+      gameData.guesses[gameData.guesses.length - 1].distanceAway;
+    distanceAwayVisual = `📍${getTicksWithDistance(
+      gameData.guesses[gameData.guesses.length - 1].distanceAway
+    )}🌎`;
   }
-
-  
-
 
   // On close of round announcement - play audio and clear previous guesses
   const handleClose = () => {
@@ -91,16 +95,24 @@ export default function Announcement(props) {
         <DialogTitle
           sx={{ m: "auto", fontFamily: "Wild World" }}
         >{`${roundTitle}`}</DialogTitle>
-        
-        {gameData.round > 1 
-        &&
-        <DialogContent sx={{ m: "1", textAlign: "center" }}>
-          <Typography>The correct answer was <strong>{answer.country}</strong>. {roundAnswer}</Typography>
-          <Typography mt={1}>The station playing was <strong>{answer.station_title}</strong> in <strong>{answer.city}</strong></Typography>
-          <Typography mt={1}>Your pin was <strong>{pinDistanceAway} km</strong> away.</Typography>
-          <Typography mt={1}>{distanceAwayVisual}</Typography>
-        </DialogContent>}
-        
+
+        {gameData.round > 1 && (
+          <DialogContent sx={{ m: "1", textAlign: "center" }}>
+            <Typography>
+              The correct answer was <strong>{currentStation.country}</strong>.{" "}
+              {roundAnswer}
+            </Typography>
+            <Typography mt={1}>
+              The station playing was <strong>{currentStation.station_title}</strong> in{" "}
+              <strong>{currentStation.city}</strong>
+            </Typography>
+            <Typography mt={1}>
+              Your pin was <strong>{pinDistanceAway} km</strong> away.
+            </Typography>
+            <Typography mt={1}>{distanceAwayVisual}</Typography>
+          </DialogContent>
+        )}
+
         <DialogActions sx={{ p: 2 }}>
           {buffer ? (
             <CircularProgress sx={{ m: "auto" }} />
