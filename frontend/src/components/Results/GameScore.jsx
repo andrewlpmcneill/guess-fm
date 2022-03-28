450de251b86fdd6f713a07d707df7a1587e41f7a
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-27 23:11:20 -0400

    add styles to game score

--- frontend/src/components/Results/GameScore.jsx
+++ frontend/src/components/Results/GameScore.jsx
@@ -8,12 +8,35 @@ export default function GameScore(props) {
   return (
     <Stack direction="column">
       <Typography
-        sx={{ fontFamily: "Wild World", marginBottom: "1rem", marginTop: "1rem", textAlign:"center", color:"#F2F2F2" }}
+        sx={{
+          fontFamily: "Wild World",
+          marginBottom: "1rem",
+          marginTop: "1.5rem",
+          textAlign: "center",
+          color: "#F2F2F2",
+          minHeight: "150px",
+          position: "relative",
+          textShadow: "3px 3px 4px black"
+  
+        }}
         variant={"h5"}
       >
-        CURRENT SCORE
+        SCORE
       </Typography>
-      <Typography sx={{ margin: "auto" }}>{score}/3</Typography>
+      <Typography
+        variant="h4"
+        sx={{ margin: "auto", fontFamily: "Wild World"}}
+      >
+        <Typography variant="h4" sx={{ fontFamily: "Wild World", position: "absolute", left: "36%", bottom: "16%", color: "#F2F2F2", textShadow: "3px 3px 4px black"}}>
+          {score}
+        </Typography>
+        <Typography variant="h5" sx={{ fontFamily: "Wild World", position: "absolute", left: "46%", bottom: "14%", color: "#F2F2F2", textShadow: "3px 3px 4px black" }}>
+          /
+        </Typography>
+        <Typography variant="h4" sx={{ fontFamily: "Wild World", position: "absolute", left: "50%", bottom: "9%", color: "#F2F2F2", textShadow: "3px 3px 4px black" }}>
+          3
+        </Typography>
+      </Typography>
     </Stack>
   );
 }
