7adb79e4521e2ea511c15f7c8d8eb862a6f0fd9a
Seamus McGill <seamusmcgill@gmail.com>
2022-03-28 12:25:13 -0400

    Adds ThemeProvider to pass fonts to all components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import Map from "./components/Map";
 import "./App.css";
+import { createTheme } from "@mui/material";
 import Player from "./components/Player/index";
 import Results from "./components/Results/index";
 import GuessButton from "./components/GuessButton";
@@ -10,6 +11,7 @@ import useGameData from "./hooks/useGameData";
 import usePlayerData from "./hooks/usePlayerData";
 import useModelData from "./hooks/useModelData";
 import NavBar from "./components/NavBar";
+import { ThemeProvider } from "@mui/material/styles";
 
 function App() {
   // DATABASE STATE AND FUNCTIONS
@@ -60,86 +62,111 @@ function App() {
     muted
   } = usePlayerData();
 
+  const theme = createTheme({
+    components: {
+      MuiTypography: {
+        defaultProps: {
+          fontFamily: "IBM Plex Mono",
+          color: "white"
+        },
+        h6: {
+          fontFamily: "Wild World",
+        }
+      },
+      MuiDialogTitle: {
+        defaultProps: {
+          backgroundColor: '#20202a',
+          color: "#c9333b",
+          m: "auto", 
+          p: 2,
+        },
+      },
+    }
+  })
+
   return (
     <div className="App">
-      <Box sx={{ position: "relative", overflow: "hidden" }}>
-        <NavBar 
-          setIsAboutOpen={setIsAboutOpen}
-          setIsStatsOpen={setIsStatsOpen}
-        />
-        <Map gameData={gameData} assignCoords={assignCoords} />
-        <Dialog
-          loadAudio={loadAudio}
-          play={play}
-          pause={pause}
-          clearGuesses={clearGuesses}
-          gameData={gameData}
-          setGameData={setGameData}
-          modelState={modelState}
-          setModelState={setModelState}
-          createGame={createGame}
-          createRound={createRound}
-          updateResultsTable={updateResultsTable}
-          clearScore={clearScore}
-          clearRound={clearRound}
-          nextRound={nextRound}
-          getGameStatistics={getGameStatistics}
-          isAboutOpen={isAboutOpen}
-          setIsAboutOpen={setIsAboutOpen}
-          isStatsOpen={isStatsOpen}
-          setIsStatsOpen={setIsStatsOpen}
-          getLifeTimeStatistics={getLifeTimeStatistics}
-          isInstructionsOpen={isInstructionsOpen}
-          setIsInstructionsOpen={setIsInstructionsOpen}
-        />
-        <Results
-          guesses={gameData.guesses}
-          gameData={gameData}
-          setGameData={setGameData}
-        />
-        <Stack
-          direction="row"
-          // m="0 2em"
-          spacing={2}
-          alignItems="center"
-          justifyContent="space-between"
-        >
-          <Player
-            volume={volume}
-            playing={playing}
-            handleClick={click}
-            handleChange={handleChange}
-            addGuess={addGuess}
-            source={source}
-            setSource={setSource}
-            setPlaying={setPlaying}
-            error={error}
-            getNewStation={getNewStation}
+      <ThemeProvider theme={theme}>
+        <Box sx={{ position: "relative", overflow: "hidden" }}>
+          <NavBar 
+            setIsAboutOpen={setIsAboutOpen}
+            setIsStatsOpen={setIsStatsOpen}
+          />
+          <Map gameData={gameData} assignCoords={assignCoords} />
+          <Dialog
+            loadAudio={loadAudio}
+            play={play}
+            pause={pause}
+            clearGuesses={clearGuesses}
             gameData={gameData}
             setGameData={setGameData}
-            play={play}
-            handleMute={handleMute}
-            muted={muted}
-            round={gameData.round}
+            modelState={modelState}
+            setModelState={setModelState}
+            createGame={createGame}
+            createRound={createRound}
+            updateResultsTable={updateResultsTable}
+            clearScore={clearScore}
+            clearRound={clearRound}
+            nextRound={nextRound}
+            getGameStatistics={getGameStatistics}
+            isAboutOpen={isAboutOpen}
+            setIsAboutOpen={setIsAboutOpen}
+            isStatsOpen={isStatsOpen}
+            setIsStatsOpen={setIsStatsOpen}
+            getLifeTimeStatistics={getLifeTimeStatistics}
+            isInstructionsOpen={isInstructionsOpen}
+            setIsInstructionsOpen={setIsInstructionsOpen}
+            theme={theme}
+          />
+          <Results
+            guesses={gameData.guesses}
+            gameData={gameData}
+            setGameData={setGameData}
           />
           <Stack
-            direction="column"
-            sx={{ backgroundColor: '#20202a', borderRadius: "15px", position: "absolute", right: "4em", bottom: "2em", p: "1em 1.5em", width: "300px" }}
+            direction="row"
+            // m="0 2em"
+            spacing={2}
+            alignItems="center"
+            justifyContent="space-between"
           >
-            <GuessButton
+            <Player
+              volume={volume}
+              playing={playing}
+              handleClick={click}
+              handleChange={handleChange}
               addGuess={addGuess}
+              source={source}
+              setSource={setSource}
+              setPlaying={setPlaying}
+              error={error}
+              getNewStation={getNewStation}
               gameData={gameData}
-              updateRoundTable={updateRoundTable}
-              loadAudio={loadAudio}
-              addScore={addScore}
-              setModelState={setModelState}
-              modelState={modelState}
               setGameData={setGameData}
-              nextRound={nextRound}
+              play={play}
+              handleMute={handleMute}
+              muted={muted}
+              round={gameData.round}
             />
+            <Stack
+              direction="column"
+              sx={{ backgroundColor: '#20202a', borderRadius: "15px", position: "absolute", right: "4em", bottom: "2em", p: "1em 1.5em", width: "300px" }}
+            >
+              <GuessButton
+                addGuess={addGuess}
+                gameData={gameData}
+                updateRoundTable={updateRoundTable}
+                loadAudio={loadAudio}
+                addScore={addScore}
+                setModelState={setModelState}
+                modelState={modelState}
+                setGameData={setGameData}
+                nextRound={nextRound}
+              />
+            </Stack>
           </Stack>
-        </Stack>
-      </Box>
+        </Box>
+      </ThemeProvider>
     </div>
   );
 }
