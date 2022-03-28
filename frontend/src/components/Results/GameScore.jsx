40db71ca39579ce9a43437322d8c4cfd0ac6260e
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-28 12:16:07 -0400

    adds styles to GameScore component

--- frontend/src/components/Results/GameScore.jsx
+++ frontend/src/components/Results/GameScore.jsx
@@ -4,38 +4,44 @@ import Typography from "@mui/material/Typography";
 
 export default function GameScore(props) {
   const score = props.data ? props.data : 0;
+  const scoreStyle = {
+    fontFamily: "Wild World",
+    textShadow: "3px 3px 4px black",
+    color: "#F2F2F2",
+    fontSize: "1.5rem"
+  };
 
   return (
     <Stack direction="column">
       <Typography
         sx={{
           fontFamily: "Wild World",
-          marginBottom: "1rem",
           marginTop: "1.5rem",
           textAlign: "center",
           color: "#F2F2F2",
-          minHeight: "150px",
+          minHeight: "50px",
           position: "relative",
-          textShadow: "3px 3px 4px black"
-  
+          textShadow: "3px 3px 4px black",
         }}
-        variant={"h5"}
+        variant="h6"
       >
         SCORE
       </Typography>
       <Typography
         variant="h4"
-        sx={{ margin: "auto", fontFamily: "Wild World"}}
+        sx={{ margin: "auto", fontFamily: "Wild World" }}
       >
-        <Typography variant="h4" sx={{ fontFamily: "Wild World", position: "absolute", left: "36%", bottom: "16%", color: "#F2F2F2", textShadow: "3px 3px 4px black"}}>
-          {score}
-        </Typography>
-        <Typography variant="h5" sx={{ fontFamily: "Wild World", position: "absolute", left: "46%", bottom: "14%", color: "#F2F2F2", textShadow: "3px 3px 4px black" }}>
-          /
-        </Typography>
-        <Typography variant="h4" sx={{ fontFamily: "Wild World", position: "absolute", left: "50%", bottom: "9%", color: "#F2F2F2", textShadow: "3px 3px 4px black" }}>
-          3
-        </Typography>
+        <Stack direction="row" alignContent="center" sx={{ gap: "0.4rem" }}>
+          <Typography sx={scoreStyle}>
+            {score}
+          </Typography>
+          <Typography sx={scoreStyle}>
+            /
+          </Typography>
+          <Typography sx={scoreStyle}>
+            3
+          </Typography>
+        </Stack>
       </Typography>
     </Stack>
   );
