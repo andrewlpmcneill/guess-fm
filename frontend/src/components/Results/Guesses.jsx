15212cf813cb305e2a0d26b316959370c07f10ac
Jimmy Chuk <jimmychuk@outlook.com>
2022-03-27 21:19:53 -0400

    adds dummy data to have tempory visuals

--- frontend/src/components/Results/Guesses.jsx
+++ frontend/src/components/Results/Guesses.jsx
@@ -5,18 +5,31 @@ import Typography from "@mui/material/Typography";
 
 export default function Guesses(props) {
   const guessesData = props.data;
+  
+  const tempData = [
+    {id: "a", country: "", distanceAway: null, direction:"", fake: true},
+    {id: "b", country: "", distanceAway: null, direction:"", fake: true},
+    {id: "c", country: "", distanceAway: null, direction:"", fake: true},
+    {id: "d", country: "", distanceAway: null, direction:"", fake: true},
+    {id: "e", country: "", distanceAway: null, direction:"", fake: true}
+  ]
 
-  const guesses = guessesData ? (
-    guessesData.map((guess) => <GuessesItem key={guess.id} data={guess} />)
-  ) : (
-    <Typography sx={{ textAlign: "center" }}>No guesses currently</Typography>
-  );
+  guessesData.forEach((guessItem, index) => {
+    tempData[index] = guessItem
+  })
+
+  const guesses = tempData.map(guess => <GuessesItem key={guess.id} data={guess} />)
+
+  // const guesses = guessesData ? (
+  //   guessesData.map((guess) => <GuessesItem key={guess.id} data={guess} />)
+  // ) : "";
+
 
   return (
     <Box sx={{ height: "70%" }}>
       <Typography
-        sx={{ fontFamily: "Wild World", textAlign: "center", marginBottom: '10px' }}
-        variant={"h4"}
+        sx={{ fontFamily: "Wild World", textAlign: "center", marginBottom: '1rem', marginTop: '1rem', color:"#F2F2F2" }}
+        variant={"h5"}
       >
         GUESSES
       </Typography>
