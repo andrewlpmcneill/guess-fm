c654ab950ecdf6a8fad9e87f6177db652ef4e9fe
Andrew McNeill <andrew@andrewmc.ca>
2022-03-27 21:15:00 -0300

    adds styling to volume icons and slider

--- frontend/src/components/Player/Volume.jsx
+++ frontend/src/components/Player/Volume.jsx
@@ -7,15 +7,15 @@ import VolumeUp from '@mui/icons-material/VolumeUp'
 
 export default function Volume(props) {
 
-  const { handleChange, volume } = props;
+  const { handleChange, volume, muted } = props;
 
   return (
 
     <Box sx={{ width: 220 }}>
       <Stack spacing={2} direction="row" sx={{ mb: 1 }} alignItems="center">
-        <VolumeDown />
-        <Slider aria-label="Volume" data-testid="slider" value={volume} onChange={handleChange} />
-        <VolumeUp />
+        <VolumeDown sx={{ color: muted ? "#721F23" : "#C9333B" }}/>
+        <Slider aria-label="Volume" data-testid="slider" value={volume} onChange={handleChange} sx={{ color: muted ? "#721F23" : "#C9333B", cursor: "pointer" }} />
+        <VolumeUp sx={{ color: muted ? "#721F23" : "#C9333B" }}/>
       </Stack>
     </Box>
   )
