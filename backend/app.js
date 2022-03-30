9b63be105a4686fb012d89f051729a1760e76a8b
Andrew McNeill <andrew@andrewmc.ca>
2022-03-29 22:01:53 -0300

    adds minor refactor (semicolons)

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,13 @@ const express = require('express');
 const morgan = require('morgan');
 const { Sequelize } = require('sequelize');
 const db = require('./models');
-const dotenv = require('dotenv').config()
+const dotenv = require('dotenv').config();
 
-const DBNAME = process.env.DBNAME
-const DBUSERNAME = process.env.DBUSERNAME
-const DBPASSWORD = process.env.DBPASSWORD
-const DBHOST = process.env.DBHOST
-const DBDIALECT = process.env.DBDIALECT
+const DBNAME = process.env.DBNAME;
+const DBUSERNAME = process.env.DBUSERNAME;
+const DBPASSWORD = process.env.DBPASSWORD;
+const DBHOST = process.env.DBHOST;
+const DBDIALECT = process.env.DBDIALECT;
 
 const PORT = 8080;
 const ENVIRONMENT = 'dev';
